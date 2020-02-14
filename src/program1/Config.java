0424e93b1f60aea9422325108358a1ec66b95694
Nathan Solomon <nbsolomon4559@gmail.com>
2020-02-14 06:29:30 -0600

    initial file structure setup

---
+++ src/program1/Config.java
@@ -0,0 +1,17 @@
+package program1;
+
+public class Config {
+    // Node Config Values
+    public static int MIN_TIME_SLICE = 1000;
+    public static int MAX_TIME_SLICE = 10000;
+
+    // Consumer Config values
+    public static int NUM_CONSUMERS = 2;
+
+    // Producer Config values
+    public static int NUM_PRODUCERS = 2;
+    public static int MIN_NUM_PROCESSES = 10;
+    public static int MAX_NUM_PROCESSES = 50;
+    public static int MIN_SLEEP_TIME = 1000;
+    public static int MAX_SLEEP_TIME = 10000;
+}
