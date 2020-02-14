8274aa381487fe5a516cd891f923b4fbebcbd987
Nathan Solomon <nbsolomon4559@gmail.com>
2020-02-14 07:12:42 -0600

    Filestructure (#1)
    * initial file structure setup
    
    * added more config values
    
    * added names of core methods
    
    * initial versionof node. Should include all needed methods and values

---
+++ src/program1/Config.java
@@ -0,0 +1,20 @@
+package program1;
+
+public class Config {
+    // Node Config Values
+    public static int MIN_TIME_SLICE = 1000;
+    public static int MAX_TIME_SLICE = 10000;
+    public static int MIN_PRIORITY = 50;
+
+    // Consumer Config values
+    public static int NUM_CONSUMERS = 2;
+
+    // Producer Config values
+    public static int NUM_PRODUCERS = 2;
+    public static int MIN_NUM_TIMES_WAKEUP = 3;
+    public static int MAX_NUM_TIMES_WAKEUP = 10;
+    public static int MIN_NUM_PROCESSES = 10;
+    public static int MAX_NUM_PROCESSES = 50;
+    public static int MIN_SLEEP_TIME = 1000;
+    public static int MAX_SLEEP_TIME = 10000;
+}
---
+++ src/program1/MinHeap.java
@@ -0,0 +1,5 @@
+package program1;
+
+public class MinHeap {
+    // Adding and removing for heaps are called insert and extract
+}
