0399e6b3ee385c6ac2b354222e522bcf90241c63
Jedimaster4559 <nbs0007@uah.edu>
2020-02-14 10:43:15 -0600

    Created Producer class and added it's manageament to the main class

--- src/program1/Program1.java
+++ src/program1/Program1.java
@@ -1,7 +1,32 @@
 package program1;
 
+import java.util.ArrayList;
+
 public class Program1 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        // Create producers
+        ArrayList<Thread> producers = new ArrayList<>();
+        for(int i = 0; i < Config.NUM_PRODUCERS; i++){
+            Producer p = new Producer(i);
+            Thread t = new Thread(p);
+            producers.add(t);
+            t.start();
+        }
+
+
+        // Check if all producers are complete
+        boolean complete = false;
+        while(!complete){
+            complete = true;
+            for(Thread t : producers){
+                if(t.isAlive()){
+                    complete = false;
+                    break;
+                }
+            }
+        }
+
+        // Notify Consumers that all producers are done
+
     }
 }
